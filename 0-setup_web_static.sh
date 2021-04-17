3cd89f90c1eb5ffd33552ee5f4872c0900576520
kimengu-david <davidmutune200@gmail.com>
2021-04-17 06:51:42 +0000

    Add task 0

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
-# Sets up a web server for deployment of web_static.
+#sets up a web servers for the deployment of web_static
 
 apt-get update
 apt-get install -y nginx
 
 mkdir -p /data/web_static/releases/test/
 mkdir -p /data/web_static/shared/
-echo "Holberton School" > /data/web_static/releases/test/index.html
+echo "Holberton School is cool" > /data/web_static/releases/test/index.html
 ln -sf /data/web_static/releases/test/ /data/web_static/current
 
 chown -R ubuntu /data/
@@ -15,21 +15,23 @@ chgrp -R ubuntu /data/
 printf %s "server {
     listen 80 default_server;
     listen [::]:80 default_server;
-    add_header X-Served-By $HOSTNAME;
     root   /var/www/html;
-    index  index.html index.htm;
+    server_name _;
+    add_header X-Served-By $HOSTNAME;
+
     location /hbnb_static {
         alias /data/web_static/current;
         index index.html index.htm;
     }
-    location /redirect_me {
-        return 301 http://cuberule.com/;
+
+    error_page 404 /custom_404.html;
+    location =  /custom_404.html  {
+                root /usr/share/nginx/html;
+                internal;
     }
-    error_page 404 /404.html;
-    location /404 {
-      root /var/www/html;
-      internal;
-    }
-}" > /etc/nginx/sites-available/default
+
+    rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4/$1 permanent;
+
+}" > /etc/nginx/sites-enabled/default
 
 service nginx restart
