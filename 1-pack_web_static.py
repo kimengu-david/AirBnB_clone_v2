5e965e11d4d2d9ebcdd5e7f2d0b691554eb89006
kimengu-david <davidmutune200@gmail.com>
2021-04-17 07:34:06 +0000

    fabfile to generate .tgz archive

---
+++ 1-pack_web_static.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+# generates a .tgz archive from the contents of web_static folder
+
+from datetime import datetime
+from fabric.api import local
+import os.path
+
+def do_pack():
+    """Create a tgz archive from web_static directory."""
+    date_time = datetime.now().strftime("%Y%m%d%H%M%S");
+    file = "versions/web_static_{}.tgz".format(date_time)
+    if os.path.isdir("versions") is False:
+        if local("mkdir -p versions").failed is True:
+            return None
+    if local("tar -cvzf {} web_static".format(file)).failed is True:
+        return None
+    return file
