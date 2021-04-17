d8e84a4b05b3a715ef2c0ca37e3a7e039017ad81
kimengu-david <davidmutune200@gmail.com>
2021-04-17 11:16:52 +0000

    Deployment with fabric lib

---
+++ 2-do_deploy_web_static.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python3
+# distributes an archive to a web server.
+from fabric.api import put
+from fabric.api import run
+import os.path
+from fabric.api import env
+
+env.hosts = ["ubuntu@34.73.36.236", "ubuntu@35.231.173.232"]
+
+
+def do_deploy(archive_path):
+    """Distributes an archive to a web server.
+        Returns false If the file doesn't exist at
+        archive_path or an error occurs
+        Otherwise returns True.
+    """
+    if os.path.isfile(archive_path) is False:
+        return False
+
+    file_name_with_ext = archive_path.split("/")[-1]
+    file_name_no_ext = file_name_with_ext.split(".")[0]
+
+    if put(archive_path, "/tmp/{}".format(file_name_with_ext)).failed is True:
+        return False
+    if run("rm -rf /data/web_static/releases/{}/".
+           format(file_name_no_ext)).failed is True:
+        return False
+    if run("mkdir -p /data/web_static/releases/{}/".
+           format(file_name_no_ext)).failed is True:
+        return False
+    if run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/".
+           format(file_name_with_ext, file_name_no_ext)).failed is True:
+        return False
+    if run("rm -r /tmp/{}".format(file_name_with_ext)).failed is True:
+        return False
+    if run("mv /data/web_static/releases/{}/web_static/* "
+           "/data/web_static/releases/{}/"
+           .format(file_name_no_ext, file_name_no_ext)).failed is True:
+        return False
+    if run("rm -rf /data/web_static/releases/{}/web_static".
+           format(file_name_no_ext)).failed is True:
+        return False
+    if run("rm -rf /data/web_static/current").failed is True:
+        return False
+    if run("ln -s /data/web_static/releases/{}/ /data/web_static/current".
+           format(file_name_no_ext)).failed is True:
+        return False
+    print("New version deployed!")
+    return True
