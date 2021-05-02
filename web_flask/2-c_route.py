164a6f0927f5e89527b7ff098170df4eb2903084
kimengu-david <davidmutune200@gmail.com>
2021-05-02 09:49:12 +0000

    URL variable rules

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+from flask import Flask
+"""
+starts a Flask web application listening on any IP port 5000.
+Routes: '/' -- Returns 'Hello HBNB'
+        '/hbnb' -- Returns the string 'HBNB'
+        '/c/<text> -- Displays "C" ffollowed by the value of the text variable.
+"""
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+    """Sends the string 'Hello BNB!' to the browser"""
+    return "Hello HBNB!"
+
+@app.route('/hbnb', strict_slashes=False)
+def hello_hbnb():
+    """Returns the string "HBNB" to the browser"""
+    return "HBNB"
+
+@app.route('/c/<text>', strict_slashes=False)
+def cisfun(text):
+    """Dispalys the string 'C' followed by the value of text variable."""
+    text = text.replace("_", " ")
+    return "C {}".format(text)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
