4e1cbd6b62aa8bd7a583322205183b17c3920e4e
kimengu-david <davidmutune200@gmail.com>
2021-05-02 10:07:29 +0000

    Flask URL variables

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -27,6 +27,7 @@ def cisfun(text):
     text = text.replace("_", " ")
     return "C {}".format(text)
 
+
 @app.route("/python", strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
 def python_is_cool(text="is cool"):
