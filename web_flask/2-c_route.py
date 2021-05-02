4b260d7c6e9abc33493444c805afa9ad8091da41
kimengu-david <davidmutune200@gmail.com>
2021-05-02 09:57:54 +0000

    Remove pep8 style error

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -14,10 +14,12 @@ def hello():
     """Sends the string 'Hello BNB!' to the browser"""
     return "Hello HBNB!"
 
+
 @app.route('/hbnb', strict_slashes=False)
 def hello_hbnb():
     """Returns the string "HBNB" to the browser"""
     return "HBNB"
+
 
 @app.route('/c/<text>', strict_slashes=False)
 def cisfun(text):
